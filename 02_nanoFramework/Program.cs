8cd42d14e97add7584d8af11df24976cddb22198
Clifford Agius <cliffordagius@ca-software.co.uk>
2021-11-10 09:29:59 +0000

    finsihed adding the Device Twins code and also the C2D message system so that we can enable emergency reactions from the data stream...

--- 02_nanoFramework/Program.cs
+++ 02_nanoFramework/Program.cs
@@ -1,6 +1,8 @@
 using Amqp;
 using nanoFramework.Azure.Devices.Client;
 using nanoFramework.Azure.Devices.Provisioning.Client;
+using nanoFramework.Azure.Devices.Shared;
+using nanoFramework.Json;
 using nanoFramework.Networking;
 using nanoFramework.Runtime.Native;
 using System;
@@ -17,27 +19,22 @@ namespace TechDays2021
     {
         // Browse our samples repository: https://github.com/nanoframework/samples
         // Check our documentation online: https://docs.nanoframework.net/
-        // Join our lively Discord community: https://discord.gg/gCyBu8T
+        // Join our lively Discord community: https://discord.gg/gCyBu8T.
 
-        // Azure IoTHub settings
-        const string _hubName = "TechDays2021";
-        const string _deviceId = "TechDays2021-Device1";    // <- Give your device a meaningful name...
-        const string _sasToken = "SharedAccessSignature sr=TechDays2021.azure-devices.net%2Fdevices%2FTechDays2021-Device1&sig=YnPUPjpCBUlPO1rqBFzSn2cOUhOEn%2FF1l0YlKtRD%2FZA%3D&se=1635172297";  // <- See blog post on how to obtain your SAS token.
-
-        //***  FOR DPS ***
-        public static string RegistrationID = "nanoFramework-01";
+        // Azure DPS/IoTHub settings
+        public static string RegistrationID = "nanoFramework-01";   //TempName will be replaced by the Flight number from the JSON packets.
         const string DpsAddress = "global.azure-devices-provisioning.net";
         const string IdScope = "0ne00426F38";
         const string SasKey = "266pldCRiFGxSXkt6QcCPkqfCf8FMFIvD6yqpi+6Jy0=";
 
-
-
-        // AMQP Tracing.
-        static bool TraceOn = false;
+        public static DeviceClient DeviceClient;
+        public static Twin DeviceTwin;
 
         // Model Data
+        private static FlightDataStore flightDataStore = new();
         private static FlightDataModel[] FlightDataModel;
         private static int counter = 0;
+        private static int Warning = 0;
 
         public static void Main()
         {
@@ -67,15 +64,7 @@ namespace TechDays2021
                 Debug.WriteLine($"YAY! Connected to Wifi...");
             }
 
-            // Setup AMQP
-            // Set trace level 
-            AmqpTrace.TraceLevel = TraceLevel.Frame | TraceLevel.Information;
-            // Enable tracing
-            AmqpTrace.TraceListener = WriteTrace;
-            Connection.DisableServerCertValidation = false;
-
             // Get the JSON Data from the SD card File...
-            FlightDataStore flightDataStore = new();
             FlightDataModel = flightDataStore.GetFlightData();
 
             if (FlightDataModel == null || FlightDataModel.Length == 0)
@@ -90,6 +79,11 @@ namespace TechDays2021
                 // Connect to DPS...
                 ConnectWithDPS();
 
+                //Add the events for C2D messages.
+                DeviceClient.CloudToDeviceMessage += DeviceClient_CloudToDeviceMessage;
+                DeviceClient.TwinUpated += DeviceClient_TwinUpated;
+                DeviceClient.AddMethodCallback(MethodCallbackStart);
+
                 // launch worker thread where the real work is done!
                 new Thread(WorkerThread).Start();
             }
@@ -101,81 +95,99 @@ namespace TechDays2021
         {
             X509Certificate azureCA = new X509Certificate(Resources.GetBytes(Resources.BinaryResources.BaltimoreRootCA_crt));
             var provisioning = ProvisioningDeviceClient.Create(DpsAddress, IdScope, RegistrationID, SasKey, azureCA);
-            var myDevice = provisioning.Register(new CancellationTokenSource(60000).Token);
+            var myDeviceRegistration = provisioning.Register(new CancellationTokenSource(60000).Token);
 
-            if (myDevice.Status != ProvisioningRegistrationStatusType.Assigned)
+            if (myDeviceRegistration.Status != ProvisioningRegistrationStatusType.Assigned)
             {
-                Debug.WriteLine($"Registration is not assigned: {myDevice.Status}, error message: {myDevice.ErrorMessage}");
+                Debug.WriteLine($"Registration is not assigned: {myDeviceRegistration.Status}, error message: {myDeviceRegistration.ErrorMessage}");
                 return false;
             }
 
             Debug.WriteLine($"Device successfully assigned:");
-            Debug.WriteLine($"  Assigned Hub: {myDevice.AssignedHub}");
-            Debug.WriteLine($"  Created time: {myDevice.CreatedDateTimeUtc}");
-            Debug.WriteLine($"  Device ID: {myDevice.DeviceId}");
-            Debug.WriteLine($"  Error code: {myDevice.ErrorCode}");
-            Debug.WriteLine($"  Error message: {myDevice.ErrorMessage}");
-            Debug.WriteLine($"  ETAG: {myDevice.Etag}");
-            Debug.WriteLine($"  Generation ID: {myDevice.GenerationId}");
-            Debug.WriteLine($"  Last update: {myDevice.LastUpdatedDateTimeUtc}");
-            Debug.WriteLine($"  Status: {myDevice.Status}");
-            Debug.WriteLine($"  Sub Status: {myDevice.Substatus}");
+            Debug.WriteLine($"  Assigned Hub: {myDeviceRegistration.AssignedHub}");
+            Debug.WriteLine($"  Created time: {myDeviceRegistration.CreatedDateTimeUtc}");
+            Debug.WriteLine($"  Device ID: {myDeviceRegistration.DeviceId}");
+            Debug.WriteLine($"  Error code: {myDeviceRegistration.ErrorCode}");
+            Debug.WriteLine($"  Error message: {myDeviceRegistration.ErrorMessage}");
+            Debug.WriteLine($"  ETAG: {myDeviceRegistration.Etag}");
+            Debug.WriteLine($"  Generation ID: {myDeviceRegistration.GenerationId}");
+            Debug.WriteLine($"  Last update: {myDeviceRegistration.LastUpdatedDateTimeUtc}");
+            Debug.WriteLine($"  Status: {myDeviceRegistration.Status}");
+            Debug.WriteLine($"  Sub Status: {myDeviceRegistration.Substatus}");
 
             // You can then create the device
-            var device = new DeviceClient(myDevice.AssignedHub, myDevice.DeviceId, SasKey, nanoFramework.M2Mqtt.Messages.MqttQoSLevel.AtMostOnce, azureCA);
+            DeviceClient = new DeviceClient(myDeviceRegistration.AssignedHub, myDeviceRegistration.DeviceId, SasKey, nanoFramework.M2Mqtt.Messages.MqttQoSLevel.AtMostOnce, azureCA);
             // Open it and continue like for the previous sections
-            var res = device.Open();
-            if (!res)
+            if (!DeviceClient.Open())
             {
                 Debug.WriteLine($"can't open the device");
+                DeviceClient.Close();
                 return false;
             }
 
-            var twin = device.GetTwin(new CancellationTokenSource(15000).Token);
+            DeviceTwin = DeviceClient.GetTwin(new CancellationTokenSource(15000).Token);
 
-            if (twin != null)
-            {
-                Debug.WriteLine($"Got twins");
-                Debug.WriteLine($"  {twin.Properties.Desired.ToJson()}");
-            }
+            Debug.WriteLine($"Twin DeviceID: {DeviceTwin.DeviceId}, #desired: {DeviceTwin.Properties.Desired.Count}, #reported: {DeviceTwin.Properties.Reported.Count}");
+
+            TwinCollection reported = new TwinCollection();
+            reported.Add("firmware", "myNano");
+            reported.Add("sdk", 0.2);
+            DeviceClient.UpdateReportedProperties(reported);
 
             return true;
+        }
+
+        string MethodCallbackStart(int rid, string payload)
+        {
+            return "Flight is closed and requesting pushback...";
         }
 
         private static void WorkerThread()
         {
             try
             {
-                // parse Azure IoT Hub Map settings to AMQP protocol settings
-                string hostName = _hubName + ".azure-devices.net";
-                string userName = _deviceId + "@sas." + _hubName;
-                string senderAddress = "devices/" + _deviceId + "/messages/events";
-                string receiverAddress = "devices/" + _deviceId + "/messages/deviceBound";
-
-                Connection connection = new Connection(new Address(hostName, 5671, userName, _sasToken));
-                Session session = new Session(connection);
-                SenderLink sender = new SenderLink(session, "send-link", senderAddress);
-                ReceiverLink receiver = new ReceiverLink(session, "receive-link", receiverAddress);
-                receiver.Start(100, OnMessage);
+                string messagePayload = "";
 
                 while (true)
                 {
-                    // Serialize the Current FlightDataModel into JSON to send as the mssage payload.
-                    string messagePayload = ""; // JsonConvert.SerializeObject(FlightDataModel[counter]);
+                    // Check if there are any warning active and if so fake the error...
+                    if (Warning != 0)
+                    {
+                        switch (Warning)
+                        {
+                            case (int)Emergency.AltitudeError:
+                                FlightDataModel[counter].altitude = 50000;
+                                break;
+                            case (int)Emergency.VerticalSpeedDive:
+                                FlightDataModel[counter].vSFPM = -6000;
+                                break;
+                            case (int)Emergency.IceCrystalIcingWarning:
+                                FlightDataModel[counter].outSideAirTemp = 0;
+                                break;
+                            default:
+                                break;
+                        }                        
+                    }
 
-                    // compose message
-                    Message message = new Message(Encoding.UTF8.GetBytes(messagePayload));
-                    message.ApplicationProperties = new Amqp.Framing.ApplicationProperties();
+                    // Serialize the Current FlightDataModel into JSON to send as the mssage payload...
+                    messagePayload = JsonConvert.SerializeObject(FlightDataModel[counter]);
 
-                    // send message with the new Lat/Lon
-                    sender.Send(message, null, null);
+
+                    // Send message to IoTHub...
+                    DeviceClient.SendMessage(messagePayload, new CancellationTokenSource(2000).Token);
 
                     // data sent
                     Debug.WriteLine($"*** DATA SENT - Next packet will be sent in {FlightDataModel[counter].secondsNextReport} seconds ***");
 
-                    // wait before sending the next position update
+                    // Wait before sending the next position update..
                     Thread.Sleep(FlightDataModel[counter].secondsNextReport);
                     counter++;
+
+                    if (FlightDataModel.Length >= counter)
+                    {
+                        // All the current data chunk has been used grab the next...
+                        FlightDataModel = flightDataStore.GetFlightData();
+                    }
                 }
             }
             catch (Exception ex)
@@ -184,27 +196,43 @@ namespace TechDays2021
             }
         }
 
-        private static void OnMessage(IReceiverLink receiver, Message message)
+        // Cloud to Device (C2D) message has been recieved and needs to be processed...
+        private static void DeviceClient_CloudToDeviceMessage(object sender, CloudToDeviceMessageEventArgs e)
         {
-            try
+            Debug.WriteLine($"*** C2D Message arrived: {e.Message} ***");
+
+            switch (e.Message)
             {
-                // command received 
-                //Double.TryParse((string)message.ApplicationProperties["setlat"], out FlightDataModel.Latitude);
-                //Double.TryParse((string)message.ApplicationProperties["setlon"], out FlightDataModel.Longitude);
-                //Debug.WriteLine($"== Received new Location setting: Lat - {Latitude}, Lon - {Longitude} ==");
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"-- C2D Error - {ex.Message} --");
+                case "AltitudeError":
+                    Warning = (int)Emergency.AltitudeError;
+                    break;
+                case "VerticalSpeedDive":
+                    Warning = (int)Emergency.VerticalSpeedDive;
+                    break;
+                case "IceCrystalIcingWarning":
+                    Warning = (int)Emergency.IceCrystalIcingWarning;
+                    break;
+                default:
+                    break;
             }
         }
 
-        static void WriteTrace(TraceLevel level, string format, params object[] args)
+        private static void DeviceClient_TwinUpated(object sender, nanoFramework.Azure.Devices.Shared.TwinUpdateEventArgs e)
         {
-            if (TraceOn)
+            if (e.Twin != null)
             {
-                Debug.WriteLine(Fx.Format(format, args));
+                Debug.WriteLine($"Got twins");
+                Debug.WriteLine($"  {e.Twin.ToJson()}");
             }
+        }
+
+        // Types of Emergency Warning that can be sent from the Cloud to the Device.
+        enum Emergency
+        {
+            None,
+            AltitudeError,          // Shift the Aircraft to 50,000ft...
+            VerticalSpeedDive,      // Make the Aircraft dive at 6000ft per minute as if it was in an emergency decent...
+            IceCrystalIcingWarning  // Make the Outside Air Temp read 0 degrees which at Cruise Altitude is an indication of Ice Crystal Icing and a real danger.
         }
     }
 }
